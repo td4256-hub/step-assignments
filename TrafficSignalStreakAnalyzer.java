5ed7557478836acccf17a6771ebd811e6cf092c1
td4256 <td4256@srmist.edu.in>
2026-09-19 10:14:45 +0530

    ok

---
+++ TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,36 @@
+public class TrafficSignalStreakAnalyzer {
+
+    public static void findLongestStreak(String signalLog) {
+
+        int currentStreak = 1;
+        int longestStreak = 1;
+
+        char longestColor = signalLog.charAt(0);
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentStreak++;
+            } else {
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println(
+            "Longest Streak: '" + longestColor +
+            "' repeated " + longestStreak + " times"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        findLongestStreak("RRGGGYRR");
+
+        findLongestStreak("RRRRYYGG");
+    }
+}
